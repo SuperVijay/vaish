d730d68fea2ec6fd7aafa748385b032643b8a15e
Canakala, Vijay (CORP) <Vijaya.Canakala@adp.com>
2018-01-01 23:40:32 -0500

    initial commit with vm practice script

---
+++ programs/OracleResumePrep.sql
@@ -0,0 +1,57 @@
+
+1. PL/SQL TOPICS
+    PRAGMA AUTONOU
+    BULK COLLECT
+    EXECUTE IMMEDIATE EXCEPTIONS
+    COLLECTIONS
+
+2. SQL TOPICS
+    COLLECT
+    MERGE
+    PIPELINE
+    ANALYTICAL
+    REGEXP_
+    HIERACHIACL
+    PIVOT
+    XMLTABLES
+    utl_http, utl_file, dbms_scheduler, dbms_mview, (partition schemes)
+
+3. 11G 12C NEW FEATURES
+    JSON SUPPORT
+    auto increment
+
+4. SQL PERFORMANCE TUNING TOPICS
+    SQL MONITORING
+    V$ VIEWS AND SQL, WORKAREA, HINTS, JOINS
+    exists in not exists (sub queries)
+    avoind/use analytical functions for tuning
+
+5. ORACLE DB ARCHITECTURE
+    MV
+    PARTITIONING
+    SCHEDULERS
+    LOBS
+    RAC env, SAN/VIP, basic networking
+    ER diagrams, norm/denorm
+    OEM familiarity
+
+
+6. BASIC ORACLE DBA COURSE (3)
+
+7. ORACLE GOLDEN GATE HANDsON + PARAMETER SIGNIFICANCE + USE CASES
+
+8. LINUX SHELL SCRIPTING
+
+9. DATA MODELLING, STAR SCHEMA (Erwin, SQL Developer data modeller)
+
+10. big data (hadoop commands, flume, hive, spark, kafka, basic mapreduce, python, monitoring)
+
+
+
+export SPARK_HOME=/Users/canakalv/Desktop/tools/spark-2.1.0-bin-hadoop2.7
+export PATH=$SPARK_HOME:$PATH
+export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH
+
+export PYSPARK_DRIVER_PYTHON="jupyter"
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
+export PYSPARK_PYTHON=python
